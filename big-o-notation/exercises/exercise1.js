085710c22825a1c0bc19fcacdd0a83c2a4640fa1
saulgavrilov <shaulgav@gmail.com>
2022-01-25 13:24:47 +0200

    Added exercises to big O :heavy_plus_sign:

---
+++ big-o-notation/exercises/exercise1.js
@@ -0,0 +1,22 @@
+// What is the Big O of the below function? (Hint, you may want to go line by line)
+
+function funChallenge(input) {
+  let a = 10; // O(1)
+  a = 50 + 3; // O(1)
+
+  for (let i = 0; i < input.length; i++) {
+    anotherFunction(); // O(n)
+    let stranger = true; // O(n)
+    a++; // O(n)
+  }
+  return a; // O(1)
+}
+
+/*
+  Time Complexity: O(n)
+
+  let's break it down
+    3 + n + n + n
+    3 + 3n
+    Big(3 + 3n) -> O(n)
+*/
