452ec93f06c19d4ee029ae6c07aa2ddff429318d
saulgavrilov <shaulgav@gmail.com>
2022-01-30 17:11:22 +0200

    Added BFS Recursive search :heavy_plus_sign:

--- searching/BFS_DFS.js
+++ searching/BFS_DFS.js
@@ -151,6 +151,21 @@ class BinarySearchTree {
 
     return list;
   }
+
+  breadthFirstSearchRecursive(queue, list) {
+    if (!queue.length) return list;
+
+    let currentNode = queue.shift();
+    list.push(currentNode.value);
+    if (currentNode.left) {
+      queue.push(currentNode.left);
+    }
+    if (currentNode.right) {
+      queue.push(currentNode.right);
+    }
+
+    return this.breadthFirstSearchRecursive(queue, list);
+  }
 }
 
 //     9
@@ -168,6 +183,7 @@ tree.insert(1);
 // tree.remove(170);
 
 console.log(tree.breadthFirstSearch());
+console.log(tree.breadthFirstSearchRecursive([tree.root], []));
 
 function traverse(node) {
   const tree = { value: node.value };
